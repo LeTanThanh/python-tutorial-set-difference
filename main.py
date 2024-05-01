c873becd5aa6690cf57b395f5882494c75bfc58e
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-01 21:02:56 +0700

    Using Python set difference operator (-) to find the difference between sets

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 if __name__ == "__main__":
   # Using Python Set difference() method to find the difference between sets
+
+  """
+  s1.difference(s2, s3, ...)
+  """
+
+  s1 = {"Python", "Java", "C++"}
+  s2 = {"C#", "Java", "C++"}
+  s = s1.difference(s2)
+
+  print(s1)
+  print(s2)
+  print(s)
+
+  # Using Python set difference operator (-) to find the difference between sets
+
+  """
+  s = s1 - s2
+  """
+
+  s1 = {"Python", "Java", "C++"}
+  s2 = {"C#", "Java", "C++"}
+  s = s1 - s2
+
+  print(s1)
+  print(s2)
+  print(s)
